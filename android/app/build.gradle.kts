ea85c21cfe27979f4265ac0acdb7bd1be535feed
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-08-30 17:16:10 -0300

    feat: Integrate Firebase into Android project
    - Add Google Services Gradle plugin to project and app build.gradle.kts files
    - Import Firebase Bill of Materials (BoM)
    - Add dependencies for Firebase Analytics, Auth, Firestore, and Storage

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
+    // Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
@@ -41,4 +43,16 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:34.2.0"))
+
+    // Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-storage")
 }
