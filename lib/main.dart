bf2e4b14d882b7564186353dde4a1a61082bd919
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-08-30 18:43:58 -0300

    Refactor: Remove unused import in main.dart
    This commit removes an unused import of `package:flutter/foundation.dart` from the `lib/main.dart` file.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
 import 'package:provider/provider.dart';
 import 'package:go_router/go_router.dart';
 import 'package:firebase_core/firebase_core.dart';
