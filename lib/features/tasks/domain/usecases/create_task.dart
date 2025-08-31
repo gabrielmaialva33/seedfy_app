291bd686b4f6637664357ff6afec33d2aa73e6b9
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-08-31 12:35:44 -0300

    Refactor: Update Task entity import alias in usecases
    The Task entity import was changed from `import '../../../../shared/domain/models/task.dart';` to `import '../../../../shared/domain/entities/task.dart' as entities;` in `lib/features/tasks/domain/usecases/get_user_tasks.dart`.
    
    This change updates the type hints in the `GetUserTasks`, `GetPendingTasks`, `GetTodayTasks`, and `GetOverdueTasks` use cases to use the aliased `entities.Task` instead of `Task`.

--- lib/features/tasks/domain/usecases/create_task.dart
+++ lib/features/tasks/domain/usecases/create_task.dart
@@ -3,22 +3,22 @@ import 'package:equatable/equatable.dart';
 
 import '../../../../core/errors/failures.dart';
 import '../../../../core/usecases/usecase.dart';
-import '../../../../shared/domain/entities/task.dart';
+import '../../../../shared/domain/entities/task.dart' as entities;
 import '../repositories/task_repository.dart';
 
-class CreateTask implements UseCase<Task, CreateTaskParams> {
+class CreateTask implements UseCase<entities.Task, CreateTaskParams> {
   final TaskRepository repository;
 
   const CreateTask(this.repository);
 
   @override
-  Future<Either<Failure, Task>> call(CreateTaskParams params) async {
+  Future<Either<Failure, entities.Task>> call(CreateTaskParams params) async {
     return await repository.createTask(params.task);
   }
 }
 
 class CreateTaskParams extends Equatable {
-  final Task task;
+  final entities.Task task;
 
   const CreateTaskParams({required this.task});
 
--- lib/features/tasks/domain/usecases/get_user_tasks.dart
+++ lib/features/tasks/domain/usecases/get_user_tasks.dart
@@ -5,46 +5,46 @@ import '../../../../core/usecases/usecase.dart';
 import '../../../../shared/domain/entities/task.dart' as entities;
 import '../repositories/task_repository.dart';
 
-class GetUserTasks implements UseCase<List<Task>, NoParams> {
+class GetUserTasks implements UseCase<List<entities.Task>, NoParams> {
   final TaskRepository repository;
 
   const GetUserTasks(this.repository);
 
   @override
-  Future<Either<Failure, List<Task>>> call(NoParams params) async {
+  Future<Either<Failure, List<entities.Task>>> call(NoParams params) async {
     return await repository.getUserTasks();
   }
 }
 
-class GetPendingTasks implements UseCase<List<Task>, NoParams> {
+class GetPendingTasks implements UseCase<List<entities.Task>, NoParams> {
   final TaskRepository repository;
 
   const GetPendingTasks(this.repository);
 
   @override
-  Future<Either<Failure, List<Task>>> call(NoParams params) async {
+  Future<Either<Failure, List<entities.Task>>> call(NoParams params) async {
     return await repository.getPendingTasks();
   }
 }
 
-class GetTodayTasks implements UseCase<List<Task>, NoParams> {
+class GetTodayTasks implements UseCase<List<entities.Task>, NoParams> {
   final TaskRepository repository;
 
   const GetTodayTasks(this.repository);
 
   @override
-  Future<Either<Failure, List<Task>>> call(NoParams params) async {
+  Future<Either<Failure, List<entities.Task>>> call(NoParams params) async {
     return await repository.getTodayTasks();
   }
 }
 
-class GetOverdueTasks implements UseCase<List<Task>, NoParams> {
+class GetOverdueTasks implements UseCase<List<entities.Task>, NoParams> {
   final TaskRepository repository;
 
   const GetOverdueTasks(this.repository);
 
   @override
-  Future<Either<Failure, List<Task>>> call(NoParams params) async {
+  Future<Either<Failure, List<entities.Task>>> call(NoParams params) async {
     return await repository.getOverdueTasks();
   }
 }
