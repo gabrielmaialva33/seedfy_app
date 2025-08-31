affb70898b7457f0dfeab94a6272fec509bc8c00
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-08-31 14:54:51 -0300

    Refactor: Remove unused provider import and add go_router to login_screen.dart

--- lib/features/auth/presentation/screens/login_screen.dart
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
 import '../../../../core/providers/locale_provider.dart';
