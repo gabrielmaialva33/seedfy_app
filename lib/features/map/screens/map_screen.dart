cac1dc80ddbb57e61e3ef941ea78a450945322a0
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-08-31 14:58:34 -0300

    Refactor: Add go_router to MapScreen
    - Add go_router import to lib/features/map/screens/map_screen.dart

--- lib/features/map/screens/map_screen.dart
+++ lib/features/map/screens/map_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
 import '../../../core/providers/locale_provider.dart';
@@ -58,7 +59,7 @@ class _MapScreenState extends State<MapScreen> {
       if (farms.isEmpty) {
         // User hasn't completed onboarding, redirect
         if (mounted) {
-          Navigator.pushReplacementNamed(context, '/onboarding');
+          context.go('/onboarding');
         }
         return;
       }
