83bedfdd92678b86a9960af9dfd8543edc3c0fe7
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-08-31 09:59:07 -0300

    fix: Add missing import for AuthBloc in MapScreen
    This commit adds the import for `AuthBloc` which was missing in `lib/features/map/screens/map_screen.dart`.

--- lib/features/map/screens/map_screen.dart
+++ lib/features/map/screens/map_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 
 import '../../../core/providers/locale_provider.dart';
+import '../../../features/auth/presentation/bloc/auth_bloc.dart';
 import '../../../shared/domain/entities/bed.dart';
 import '../../../shared/domain/entities/crop.dart';
 import '../../../shared/domain/entities/farm.dart';
