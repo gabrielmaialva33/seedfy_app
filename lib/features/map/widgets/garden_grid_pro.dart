aef0cef722cec8ed911079bbdc7ec5d57297072d
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-08-31 08:49:26 -0300

    feat: Integrate vector_math for advanced garden grid calculations
    This commit introduces the `vector_math` package to `garden_grid_pro.dart` to support more complex geometric calculations and enable future enhancements in garden bed representation and interaction.

--- lib/features/map/widgets/garden_grid_pro.dart
+++ lib/features/map/widgets/garden_grid_pro.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
 
 import '../../../core/providers/locale_provider.dart';
 import '../../../models/plot.dart';
