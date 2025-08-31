0577b6d2c72990ff17c774ad0f0f2860260c74b8
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-08-31 19:57:17 -0300

    Refactor: Remove unused Provider import
    Removed the unused `provider` import from `lib/features/auth/presentation/screens/signup_screen.dart`.

--- lib/features/auth/presentation/screens/signup_screen.dart
+++ lib/features/auth/presentation/screens/signup_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-import 'package:provider/provider.dart';
 import 'package:brasil_fields/brasil_fields.dart';
 
 import '../../../../core/providers/locale_provider.dart';
